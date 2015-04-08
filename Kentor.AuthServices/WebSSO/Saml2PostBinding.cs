6ede3ae81f404c368f2d19461d6ba0a48ff091cf
vsavic <vladica.savic@gmail.com>
2015-04-08 12:08:31 -0400

    Update Saml2PostBinding.cs
    If you tried to use HttpPost binding, Firefox will raise exception because "document.forms" is "undefined" and we will end up with JS error, and page won't be submitted.

--- Kentor.AuthServices/WebSSO/Saml2PostBinding.cs
+++ Kentor.AuthServices/WebSSO/Saml2PostBinding.cs
@@ -63,14 +63,14 @@ namespace Kentor.AuthServices.WebSso
 <!DOCTYPE html PUBLIC ""-//W3C//DTD XHTML 1.1//EN""
 ""http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"">
 <html xmlns=""http://www.w3.org/1999/xhtml"" xml:lang=""en"">
-<body onload=""document.forms[0].submit()"">
+<body onload=""document.getElementById('mainForm').submit()"">
 <noscript>
 <p>
 <strong>Note:</strong> Since your browser does not support JavaScript, 
 you must press the Continue button once to proceed.
 </p>
 </noscript>
-<form action=""{0}"" 
+<form id=""mainForm"" action=""{0}"" 
 method=""post"">
 <div>
 <input type=""hidden"" name=""{1}"" 
